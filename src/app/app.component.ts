e1de32d29ec58600b53deb6f7df6f91ef1da561a
Chris McMenomy <chris.mcmenomy@gmail.com>
2019-10-31 07:48:37 -0500

    Primary functionality complete.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   priceForm: FormGroup;
   showError: boolean;
-  cents = 0;
+  cents: number;
 
   result: string =  null;
 
@@ -37,8 +37,8 @@ export class AppComponent implements OnInit {
     this.showError = !this.validSubmission(priceInput);
     if (!this.showError) {
       // Remove any user-entered commas first.
-      // console.log(this.buildTextString(priceInput.replace(/,/g, '')));
-      this.result = this.buildTextString(priceInput.replace(/,/g, ''));
+      const commaGroups = this.formatCommaGroups(priceInput.replace(/,/g, ''));
+      this.result = this.buildFullString(commaGroups);
     }
   }
 
@@ -64,34 +64,180 @@ export class AppComponent implements OnInit {
     return true;
   }
 
-  buildTextString(price: string): string {
+  // currently just breaking down the input and formatting the dollars into accurate comma groups
+  // still need to orchestrate counting the number of comma groups and adding the correct number text
+  // after the groups
+  formatCommaGroups(price: string): string[] {
     const dollars = price.split('.')[0];
     const cents = price.split('.')[1];
     this.cents = cents ? parseInt(cents, 10) : 0;
-    console.log(this.cents);
 
-
-
-
+    // Insert commas in the correct places in order to later split the number into comma groups
+    // Reversed to group into 3's starting from the end of the number.
     let insertions = 0;
     const reversedPrice = dollars.split('').reverse();
-
     for (let i = 1; i <= reversedPrice.length; i++) {
       if (
+        // every third place
         i / 3 >= 1 &&
+        // no remainder
         i % 3 === 0 &&
+        // not at the end of the string
         i + insertions < reversedPrice.length
       ) {
         reversedPrice.splice(i + insertions, 0, ',');
         insertions++;
       }
     }
+    // Reverse the array back again, join, and split on the new commas
+    return reversedPrice.reverse().join('').split(',');
+  }
 
-    return reversedPrice.reverse().join('');
+  convertOnesPlace(digit: number): string {
+    const onesMapping = {
+      0: '',
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine'
+    };
+    return onesMapping[digit];
+  }
+
+  convertTensPlace(digit: number): string {
+    const tensMapping = {
+      0: '',
+      2: 'twenty',
+      3: 'thirty',
+      4: 'forty',
+      5: 'fifty',
+      6: 'sixty',
+      7: 'seventy',
+      8: 'eighty',
+      9: 'ninety'
+    };
+    return tensMapping[digit];
+  }
+
+  convertTeenNumbers(digits: number): string {
+    const teensMapping = {
+      10: 'ten',
+      11: 'eleven',
+      12: 'twelve',
+      13: 'thirteen',
+      14: 'fourteen',
+      15: 'fifteen',
+      16: 'sixteen',
+      17: 'seventeen',
+      18: 'eighteen',
+      19: 'nineteen',
+    };
+    return teensMapping[digits];
+  }
+
+  convertHundredsPlace(digit: number): string {
+    return this.convertOnesPlace(digit) + ' hundred';
+  }
+
+  buildFullString(commaGroups: string[]): string {
+    // takes in the commagroups array
+    // iterate through and call another function to build the string piece for the comma group
+    // add whatever (million, thousand, etc.) to the end of the number group based on position in array
+
+    const finalString: string[] = [];
+    for (let i = 0; i < commaGroups.length; i++) {
+      finalString.push(this.buildCommaGroupString(commaGroups[i], (commaGroups.length - 1 - i)));
+    }
+
+    finalString.push(this.appendCentsString());
+
+    finalString[0] = finalString[0].charAt(0).toUpperCase() + finalString[0].slice(1);
+    return finalString.join(' ');
+  }
+
+  buildCommaGroupString(commaGroup: string, remainingGroups: number): string {
+    // remaining groups is length-1 less current index
+    const groupValues = {
+      0: '',
+      1: 'thousand',
+      2: 'million',
+      3: 'billion',
+      4: 'trillion',
+      5: 'quadrillion',
+      6: 'quintillion',
+      7: 'sextillion',
+      8: 'septillion',
+      9: 'octillion',
+      10: 'nonillion'
+    };
+
+    const groupString: string[] = [];
+    // flag to denote that a number 10-19 was present, and to skip the ones place conversion
+    let teenNumberUsed = false;
+
+    for (let i = 0; i < commaGroup.length; i++) {
+      switch (commaGroup.length) {
+        // The comma group has all 3 digits
+        case 3:
+          switch (i) {
+            case 0:
+              groupString.push(this.convertHundredsPlace(parseInt(commaGroup[i], 10)));
+              break;
+            case 1:
+              if (commaGroup[i] === '1') {
+                groupString.push(this.convertTeenNumbers(parseInt(commaGroup[i] + commaGroup[i + 1], 10)));
+                teenNumberUsed = true;
+              } else {
+                groupString.push(this.convertTensPlace(parseInt(commaGroup[i], 10)));
+              }
+              break;
+            case 2:
+              if (!teenNumberUsed) {
+                groupString.push(this.convertOnesPlace(parseInt(commaGroup[i], 10)));
+              }
+              break;
+          }
+          break;
+        // The comma group is only 2 digits
+        case 2:
+          switch (i) {
+            case 0:
+              if (commaGroup[i] === '1') {
+                groupString.push(this.convertTeenNumbers(parseInt(commaGroup[i] + commaGroup[i + 1], 10)));
+                teenNumberUsed = true;
+              } else {
+                groupString.push(this.convertTensPlace(parseInt(commaGroup[i], 10)));
+              }
+              break;
+            case 1:
+              if (!teenNumberUsed) {
+                groupString.push(this.convertOnesPlace(parseInt(commaGroup[i], 10)));
+              }
+              break;
+          }
+          break;
+        // The comma group is only one digit
+        case 1:
+          groupString.push(this.convertOnesPlace(parseInt(commaGroup[i], 10)));
+          break;
+      }
+    }
+
+    groupString.push(groupValues[remainingGroups]);
+    return groupString.join(' ');
+  }
+
+  appendCentsString() {
+    return `and ${this.cents}/100 dollars`;
   }
 }
 
-
-// account for error stating only 2 cent numbers
 // styling
 // display history
+// more robust to add hyphens where needed
+// support larger numbers
