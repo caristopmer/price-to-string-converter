4c38b1b17c012926ff12271761b65e3f667cc0e6
Chris McMenomy <chris.mcmenomy@gmail.com>
2019-10-30 20:09:52 -0500

    Basic number input validation and scrubbing done. Ready to start converting to text.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'price-to-string-converter'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('price-to-string-converter');
-  });
+  // it(`should have as title 'price-to-string-converter'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('price-to-string-converter');
+  // });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('price-to-string-converter app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('price-to-string-converter app is running!');
+  // });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'price-to-string-converter';
+export class AppComponent implements OnInit {
+  priceForm: FormGroup;
+  showError: boolean;
+  cents = 0;
+
+  result: string =  null;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  // input validation
+  // - should be only numbers, commas, and one decimal point
+  // display any errors
+
+  // break off the cents and store in the component
+  // strip out any commas
+  // break the price into an array of groups of 3, starting from the end
+  // need a function to evaluate up to a 3 digit number into text
+  // (maybe have another for 2 digit that could be used by the 3 digit one as well)
+  // depending one how many groups there are, append thousand or hundred to each group string
+
+  ngOnInit() {
+    this.priceForm = this.formBuilder.group({ price: [null, Validators.required] });
+  }
+
+  priceSubmitted() {
+    const priceInput = this.priceForm.controls.price.value;
+    this.showError = !this.validSubmission(priceInput);
+    if (!this.showError) {
+      // Remove any user-entered commas first.
+      // console.log(this.buildTextString(priceInput.replace(/,/g, '')));
+      this.result = this.buildTextString(priceInput.replace(/,/g, ''));
+    }
+  }
+
+  validSubmission(price: string): boolean {
+    // Check to make sure input consists of only valid characters
+    const validCharacters = '0123456789,.';
+    for (const char of price.split('')) {
+      if (!validCharacters.includes(char)) {
+        return false;
+      }
+    }
+
+    // Check that there is only one decimal point included, and at most 2 digits of cents.
+    const dollarsAndCentsArray = price.split('.');
+    if (dollarsAndCentsArray.length > 2) {
+      return false;
+    } else if (dollarsAndCentsArray.length === 2) {
+      if (dollarsAndCentsArray[1].length > 2) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  buildTextString(price: string): string {
+    const dollars = price.split('.')[0];
+    const cents = price.split('.')[1];
+    this.cents = cents ? parseInt(cents, 10) : 0;
+    console.log(this.cents);
+
+
+
+
+    let insertions = 0;
+    const reversedPrice = dollars.split('').reverse();
+
+    for (let i = 1; i <= reversedPrice.length; i++) {
+      if (
+        i / 3 >= 1 &&
+        i % 3 === 0 &&
+        i + insertions < reversedPrice.length
+      ) {
+        reversedPrice.splice(i + insertions, 0, ',');
+        insertions++;
+      }
+    }
+
+    return reversedPrice.reverse().join('');
+  }
 }
+
+
+// account for error stating only 2 cent numbers
+// styling
+// display history
